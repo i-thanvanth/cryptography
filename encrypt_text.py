e21f3f1e020f5261d3fb8f5b5babce55471edfda
i-thanvanth <40958787+i-thanvanth@users.noreply.github.com>
2021-05-11 13:02:09 +0530

    added encrypt/decrypt text option

--- encrypt_text.py
+++ encrypt_text.py
@@ -1,6 +1,29 @@
 from Crypto.PublicKey import RSA
-print("Choose from the following...","\n")
+from Crypto.Cipher import PKCS1_OAEP
 pr_key = RSA.import_key(open('pri_key.pem', 'r').read())
 pu_key = RSA.import_key(open('pub_key.pem', 'r').read())
-print("The pr key is: ",pr_key.exportKey(),"\n The pu key is: ", pu_key.exportKey())
+def func():
+    print("\n\n Choose from the following...","\n 1) Encrypt text","\n 2) Decrypt text","\n 3) EXIT")
+    option=int(input("Enter your option:"))
+    if option == 1:
+        cipher = PKCS1_OAEP.new(key=pu_key)
+        message = bytes(input("ENTER THE STRING TO BE ENCRYPTED: "),'utf-8')
+        encrypted_text = cipher.encrypt(message)
+        print("\n",encrypted_text)
+        f = open("encrypted_text.txt", "wb")
+        f.write(encrypted_text)
+        f.close()
+        print(" The encrypted text will be written in a text file called encrypted_text.txt \n USE THE PUBLIC KEY TO ENCRYPT THE FILES","\n SEND THE PRIVATE KEY 'ONLY TO THE RECIPIENTS' ")
+        func()
+    if option == 2:
+        decrypt = PKCS1_OAEP.new(key=pr_key)
+        f = open(input("Enter the path of the file with the encrypted text: "), "rb")
+        encrypted_text=f.read()
+        f.close()
+        decrypted_text = decrypt.decrypt(encrypted_text)
+        print("\nThe decrypted message is: ",str(decrypted_text))
+        func()
+    if option == 3:
+        print("Thanks for using this app!")
+func()
 
--- run.py
+++ run.py
@@ -1,19 +1,24 @@
 import os
+print("WELCOME TO T'S ENCRYPTION\DECRYPTION APP!")
 #installing the required packages...
 print("\nInstalling the required packages...","\n")
 os.system('pip install pycryptodome')
 from Crypto.PublicKey import RSA
-print("\n Choose one from the following options")
-print("1) Create new private and public keys (ONLY USE IT THE FIRST TIME)")
-print("2) Encrypt\Decrypt text files","\n")
-option = int(input("Enter your option:"))
-if option == 1:
-    private_key = RSA.generate(2048)
-    f = open("pri_key.pem", "wb")
-    f.write(private_key.export_key())
-    f.close()
-    f = open("pub_key.pem", "wb")
-    f.write(private_key.publickey().export_key())
-    f.close()
-elif option == 2:
-    os.system("python encrypt_text.py")
+def func():
+    print("\n Choose one from the following options","\n 1) Create new private and public keys (ONLY USE IT THE FIRST TIME)","\n 2) Encrypt\Decrypt text files","\n 3) EXIT")
+    option = int(input("Enter your option:"))
+    if option == 1:
+        private_key = RSA.generate(2048)
+        f = open("pri_key.pem", "wb")
+        f.write(private_key.export_key())
+        f.close()
+        f = open("pub_key.pem", "wb")
+        f.write(private_key.publickey().export_key())
+        f.close()
+        func()
+    elif option == 2:
+        os.system("python encrypt_text.py")
+    elif option == 3:
+        print("Thanks for using T's app")
+    return None
+func()
